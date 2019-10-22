52cbc21f79916999e5d2a19d228a822904c7ffd6
Mike. Shi <ashida2016@outlook.com>
2019-10-22 17:16:36 +0800

    created

---
+++ poker_ver1.py
@@ -0,0 +1,110 @@
+# -*- coding: UTF-8 -*-
+
+# author by : Archier
+# created:  2019.10.19
+# Updated:  2019.10.20
+
+# import some external moduls
+import random
+import codecs
+import copy
+
+# --------------------------------------------------------
+# Phase 1:  make a new deck of playing cards
+# --------------------------------------------------------
+# initialize
+#cardJokers = ('Red Joker','Black Joker')
+#cardJokers = ('RJ','BJ')
+cardJokers = ('♞', '♘')
+
+#cardMarks = ('Heart','Spade','Club','Diamond')
+#cardMarks = ('S','H','D','C')
+cardMarks = ('♠', '♥', '♦','♣')
+
+cardNumbers = ('2', '3', '4', '5', '6', '7', '8',
+               '9', '10', 'J', 'Q', 'K', 'A')
+
+# make a deck of playing card
+deck = []
+
+# add jokers first
+for c in cardJokers:
+    deck.append(c)
+
+# add 4x13 cards
+for cn in cardNumbers:
+    for cm in cardMarks:
+        card = cm + cn
+        deck.append(card)
+
+print(len(deck))
+print(deck)
+
+# record a new deck to a text file
+file_name = "deck-new-54.txt"
+f = codecs.open(file_name, "w", "utf-8")
+#f = open(file_name, 'w')
+for card in deck:
+    f.write(card)
+    f.write('\n')
+f.close()
+
+# start shuffle
+print('--- cutting line ----')
+
+'''
+# method-1 of shuffle
+shuffled_deck = []
+for times in range(0, len(deck)):
+    picked_card = random.choice(deck)
+    shuffled_deck.append(picked_card)
+    deck.remove(picked_card)
+'''
+
+# method-2 of shuffle
+shuffled_deck = copy.copy(deck)
+random.shuffle(shuffled_deck)
+
+print(shuffled_deck)
+print(deck)
+
+# record a shuffled deck to a text file
+file_name = "deck-shuffle-54.txt"
+f = codecs.open(file_name, "w", "utf-8")
+#f = open(file_name, 'w')
+for card in shuffled_deck:
+    f.write(card)
+    f.write('\n')
+f.close()
+
+# start to deal
+print('--- cutting line ----')
+
+num_of_players = 3
+cards_of_player = int(len(shuffled_deck) / num_of_players)
+print('We have %d players, every one has %d cards.'
+      % (num_of_players, cards_of_player))
+
+player1_deck = []
+player2_deck = []
+player3_deck = []
+
+for times in range(cards_of_player):
+    picked_card = random.choice(shuffled_deck)
+    player1_deck.append(picked_card)
+    shuffled_deck.remove(picked_card)
+
+    picked_card = random.choice(shuffled_deck)
+    player2_deck.append(picked_card)
+    shuffled_deck.remove(picked_card)
+
+    picked_card = random.choice(shuffled_deck)
+    player3_deck.append(picked_card)
+    shuffled_deck.remove(picked_card)
+
+print('\nPlayer 1-cards is :')
+print(player1_deck)
+print('\nPlayer 2-cards is :')
+print(player2_deck)
+print('\nPlayer 3-cards is :')
+print(player3_deck)
